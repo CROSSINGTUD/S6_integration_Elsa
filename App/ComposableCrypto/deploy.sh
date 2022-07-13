9a3b3f14f5dc1b7447d48d284b0c67b32a9b098b
Julius Hardt <julius@postfach-hardt.de>
2022-07-13 08:51:10 +0200

    Update deployment script

--- App/ComposableCrypto/deploy.sh
+++ App/ComposableCrypto/deploy.sh
@@ -1,4 +1,4 @@
 ﻿#!/bin/bash
 
 dotnet.exe publish -c Release
-rsync --delete -ave ssh bin/Release/net5.0/publish/wwwroot/ root@julius-hardt.de:/var/www/html/ComposableCrypto
+rsync --delete -ave ssh bin/Release/net6.0/publish/wwwroot/ root@julius-hardt.de:/var/www/html/ComposableCrypto
